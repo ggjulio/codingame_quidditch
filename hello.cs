d2fc742a43dcbf2aa478089b9b126ae8cc3baf37
ggjulio <juligonz@student.42.fr>
2020-04-18 19:50:34 +0200

    getting started

---
+++ hello.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Numerics;
+
+class Hello
+{
+    static void Main()
+    {
+        System.
+        Vector2 vr = new Vector2(1, 3);
+        Console.Write("Shit ! \n");
+    }
+}
