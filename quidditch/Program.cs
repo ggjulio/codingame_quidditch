844f61b78bcfd037689f41c9c40356d243254d82
ggjulio <juligonz@student.42.fr>
2020-04-19 20:39:58 +0200

    .

--- quidditch/Program.cs
+++ quidditch/Program.cs
@@ -409,6 +409,32 @@ public class Strategy
 		return (false);
 	}
 
+	public static Vector2 InterceptOnCircle(p1,p2,c,r){
+    //p1 is the first line point
+    //p2 is the second line point
+    //c is the circle's center
+    //r is the circle's radius
+
+    var p3 = {x:p1.x - c.x, y:p1.y - c.y} //shifted line points
+    var p4 = {x:p2.x - c.x, y:p2.y - c.y}
+
+    var m = (p4.y - p3.y) / (p4.x - p3.x); //slope of the line
+    var b = p3.y - m * p3.x; //y-intercept of line
+
+    var underRadical = Math.pow((Math.pow(r,2)*(Math.pow(m,2)+1)),2)-Math.pow(b,2)); //the value under the square root sign 
+
+    if (underRadical < 0){
+    //line completely missed
+        return false;
+    } else {
+        var t1 = (-2*m*b+2*Math.sqrt(underRadical))/(2 * Math.pow(m,2) + 2); //one of the intercept x's
+        var t2 = (-2*m*b-2*Math.sqrt(underRadical))/(2 * Math.pow(m,2) + 2); //other intercept's x
+        var i1 = {x:t1,y:m*t1+b} //intercept point 1
+        var i2 = {x:t2,y:m*t2+b} //intercept point 2
+        return [i1,i2];
+    }
+}
+
 	public static void Attack(Game game, Wizard wizard)
 	{
 		Snaffle s = null;
